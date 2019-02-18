e243a550109e0b3f3e04f2ef9d7ec3406305f77d
Antony <34961906+Antstoe@users.noreply.github.com>
2019-02-17 20:13:57 -0800

    Commit friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,15 @@
+var friends = [{
+    "name": "Ahmed",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
 
+},
+{
+    "name": "Testing",
+    "photo": "https://www.in.gov/pla/images/internal-3.jpg",
+    "scores": [5, 1, 4, 4, 5, 1, 2, 1, 3, 2]
+}
+
+]
+
+module.exports = friends;
